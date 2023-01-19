f39e0d57c889d3fa7464175c093f59382202628a
tuongnlc <tuong.nlc@carehealth.io>
2023-01-19 16:00:05 +0700

    pattern for gcp vm

---
+++ gcp_vm/main.py
@@ -0,0 +1,47 @@
+import json
+
+
+def hello_server(name, network):
+    return {
+        'resource': [
+            {
+                'google_compute_instance': [
+                    {
+                        name: [
+                            {
+                                'allow_stopping_for_update': True,
+                                'zone': 'us-central1-a',
+                                'boot_disk': [
+                                    {
+                                        'initialize_params': [
+                                            {
+                                                'image': 'ubuntu-1804-lts'
+                                            }
+                                        ]
+                                    }
+                                ],
+                                'machine_type': 'e2-micro',
+                                'name': name,
+                                'network_interface': [
+                                    {
+                                        'network': network
+                                    }
+                                ],
+                                'labels': {
+                                    'name': name,
+                                    'purpose': 'manning-infrastructure-as-code'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+
+if __name__ == "__main__":
+    config = hello_server(name='test-vm', network='default')
+
+    with open('main.tf.json', 'w') as outfile:
+        json.dump(config, outfile, sort_keys=True, indent=4)
